a0ede6942658ab9615103a7c4c36ad7406dbee3c
prabhaschinthala1094 <117033366+prabhaschinthala1094@users.noreply.github.com>
2024-01-27 13:21:58 +0530

    Time: 2518 ms (6.59%), Space: 11.6 MB (42.31%) - LeetHub

---
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int dp[1001][1001];
+    int solve(int n,int k) {
+        if(n==0 && k==0)return 1;
+        if(n==0)return 0;
+        if(dp[n][k]!=-1)return dp[n][k];
+        int ans=0;
+        for(int i=0;i<n&&i<=k;i++){
+            ans = (ans%mod + solve(n-1,k-i)%mod)%mod;
+        }
+        return dp[n][k]=(ans)%mod;
+    }
+    int kInversePairs(int n, int k) {
+        if(k==0)return 1;
+        memset(dp,-1,sizeof(dp));
+        // cout<<dp[239][413]<<endl;
+       return solve(n,k)%mod;
+    }
+};
