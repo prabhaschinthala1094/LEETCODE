c4ed8d0fe60c9879b6786b1d87c528fb53b9e89e
prabhaschinthala1094 <117033366+prabhaschinthala1094@users.noreply.github.com>
2024-01-27 13:42:49 +0530

    Time: 2500 ms (6.59%), Space: 17.4 MB (13.19%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     int mod = 1e9+7;
-    int dp[1001][1001];
-    int solve(int n,int k) {
-        if( k==0)return 1;
-        if(n==0)return 0;
-        if(dp[n][k]!=-1)return dp[n][k];
-        int ans=0;
-        for(int i=0;i<n&&i<=k;i++){
-            ans = (ans%mod + solve(n-1,k-i)%mod)%mod;
-        }
-        return dp[n][k]=(ans)%mod;
-    }
+    
+    // int solve(int n,int k) {
+    //     if( k==0)return 1;
+    //     if(n==0)return 0;
+    //     if(dp[n][k]!=-1)return dp[n][k];
+    //     int ans=0;
+    //     for(int i=0;i<n&&i<=k;i++){
+    //         ans = (ans%mod + solve(n-1,k-i)%mod)%mod;
+    //     }
+    //     return dp[n][k]=(ans)%mod;
+    // }
     int kInversePairs(int n, int k) {
         if(k==0)return 1;
-        memset(dp,-1,sizeof(dp));
-        // cout<<dp[239][413]<<endl;
-       return solve(n,k)%mod;
+        vector<vector<int>>dp(n+1,vector<int>(k+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=k;j++){
+                if(j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                for(int p=0;p<i && p<=j;p++){
+                    dp[i][j]=(dp[i][j]%mod+dp[i-1][j-p]%mod)%mod;
+                }
+            }
+        }
+       
+        return dp[n][k];
     }
 };
