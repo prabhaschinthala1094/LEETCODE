80c22293d44fbba35f9e1fee087847ad71439ceb
prabhaschinthala1094 <117033366+prabhaschinthala1094@users.noreply.github.com>
2024-01-31 19:28:51 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (39.09%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int dp[101][101][201];
+    int dp[101][101];
     bool solve(int i,int j,int k,string &s1, string &s2, string &s3) {
        if(i>s1.size() || j>s2.size())return 0;
         if(i==s1.size() && j==s2.size()){
             if(k==s3.size())return 1;
             return 0;
         }
-        if(dp[i][j][k]!=-1)return dp[i][j][k];
+        if(dp[i][j]!=-1)return dp[i][j];
         bool ans=0;
         if(s3[k]==s2[j] && s3[k]==s1[i]){
             ans=(ans|solve(i+1,j,k+1,s1,s2,s3)) | solve(i,j+1,k+1,s1,s2,s3);
@@ -18,9 +18,9 @@ public:
         else if(s3[k]==s2[j]){
             ans=ans | solve(i,j+1,k+1,s1,s2,s3);
         }
-        else return dp[i][j][k]=0;
+        else return dp[i][j]=0;
         
-        return dp[i][j][k]=ans;
+        return dp[i][j]=ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
         // if(s1.size()==0 && s2.size()==0){
