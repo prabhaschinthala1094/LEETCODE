d820e28f85f71d56d9939deb09d40aba6b6e561c
prabhaschinthala1094 <117033366+prabhaschinthala1094@users.noreply.github.com>
2024-02-01 11:48:31 +0530

    Time: 100 ms (26.52%), Space: 53.8 MB (5.02%) - LeetHub

---
+++ 1311-get-watched-videos-by-your-friends/1311-get-watched-videos-by-your-friends.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    
+    vector<string> watchedVideosByFriends(vector<vector<string>>& watchedVideos, vector<vector<int>>& friends, int id, int level) {
+        int n=watchedVideos.size();
+       queue<pair<int,int>>q;
+        unordered_map<int,unordered_set<int>>mp;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<friends[i].size();j++){
+                
+                mp[i].insert(friends[i][j]);
+                mp[friends[i][j]].insert(i);
+                
+            }
+        }
+        for(auto it:mp){
+            cout<<it.first<<"->";
+            for(auto p:it.second){
+                cout<<p<<",";
+            }
+            cout<<endl;
+        }
+        q.push({0,id});
+        vector<int>vis(n,0);
+        vis[id]=1;
+        vector<int>frnds;
+        while(!q.empty()){ 
+            auto [currlvl,currId] = q.front();
+            q.pop();
+            if(currlvl == level){
+                frnds.push_back(currId);
+            }
+            for(auto it:mp[currId]){
+                if(!vis[it]){
+                    q.push({currlvl+1,it});
+                    vis[it]=1;
+                }
+            }
+        }
+        unordered_map<string,int>freq;
+        for(auto it:frnds){
+            for(auto p:watchedVideos[it]){
+                freq[p]++;
+            }
+        }
+        vector<pair<int,string>>ans;
+        for(auto it:freq){
+            ans.push_back({it.second,it.first});
+        }
+        sort(ans.begin(),ans.end());
+        vector<string>last;
+        for(auto it:ans){
+            last.push_back(it.second);
+        }
+        return last;
+    }
+};
