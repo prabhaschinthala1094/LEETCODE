7212bf720c9bfc682670c5c7e80727d815aa105c
prabhaschinthala1094 <117033366+prabhaschinthala1094@users.noreply.github.com>
2024-12-14 23:07:37 +0530

    Time: 109 ms (73.63%), Space: 105.5 MB (77.36%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    using pi = pair<int,int>;
     long long findScore(vector<int>& nums) {
         int n = nums.size();
-        vector<pair<int,int>>v;
-        for(int i=0;i<n;i++)v.push_back({nums[i],i});
-        sort(v.begin(),v.end());
+        priority_queue<pi,vector<pi>,greater<pi>>minh;
+        for(int i=0;i<n;i++)minh.push({nums[i],i});
         vector<bool>vis(n,0);
         long long ans = 0;
-        for(int i=0;i<n;i++){
-            int ind = v[i].second;
+        while(!minh.empty()){
+            auto [val,ind] = minh.top();
+            minh.pop();
             if(!vis[ind]){
-                ans += v[i].first;
+                ans += val;
                 vis[ind] = 1;
                 if(ind>0)vis[ind-1]= 1;
                 if(ind<n-1)vis[ind+1]=1;
